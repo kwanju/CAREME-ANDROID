1cb26d2d17104804027e3de4346ff5d22ee92c24
HaesungJeon <eternalasset@ajou.ac.kr>
2019-05-13 18:48:51 +0900

    게시글 출력 시작

---
+++ app/src/main/java/zangdol/careme/bulletinBoard/PostActivity.java
@@ -0,0 +1,16 @@
+package zangdol.careme.bulletinBoard;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import zangdol.careme.R;
+
+public class PostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
