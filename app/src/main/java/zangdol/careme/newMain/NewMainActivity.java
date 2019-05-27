e6c0b03f66b0938e1060d099f9463e0947d764d1
HaesungJeon <eternalasset@ajou.ac.kr>
2019-05-27 18:44:45 +0900

    메인화면 NewMainActivity로 작성 시작

---
+++ app/src/main/java/zangdol/careme/newMain/NewMainActivity.java
@@ -0,0 +1,18 @@
+package zangdol.careme.newMain;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import zangdol.careme.R;
+
+public class NewMainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newmain);
+    }
+
+
+}
